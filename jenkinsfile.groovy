63fca41ccbe76c7dbd3c4c7d617ed12c9927a762
Sunny Agrawal <sunny.agrawal@wipro.com>
2021-04-04 20:02:27 +0100

    added jenkinsfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,10 +3,13 @@ pipeline {
     tools {
         maven '3.6.3'
     }
+    environment {
+        SONARQUBE = credentials('sonar')
+    }
     stages {
         stage('Build') {
             steps {
-                sh 'mvn clean package sonar:sonar -s settings.xml -Dmaven.repo.local=$WORKSPACE/.m2'
+                sh 'mvn clean package sonar:sonar -s settings.xml -Dmaven.repo.local=$WORKSPACE/.m2 -Dsonar.host.url=http://192.168.0.22:9000 -Dsonar.login=$SONARQUBE_USR -Dsonar.password=$SONARQUBE_PSW'
             }
         }
         stage('Build Docker Image') {
